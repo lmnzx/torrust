6cbce854b844c5a6235ac0644d164edea1804b2e
Sayan Mallick <snyxmk@gmail.com>
2023-10-23 18:27:02 +0530

    decode bencoeded strings

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,12 @@ use serde_json;
 use std::env;
 
 fn decode_dencode_value(encoded_value: &str) -> serde_json::Value {
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        let colon_index = encoded_value.find(":").unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
+    if let Some((len, rest)) = encoded_value.split_once(':') {
+        if let Ok(len) = len.parse::<usize>() {
+            return serde_json::Value::String(rest[..len].to_string());
+        }
     }
+    panic!("Unhandled encoded value: {}", encoded_value)
 }
 
 fn main() {
