a96f0a0c5fe565a8ea315fa582e9a79deb549fcc
Sayan Mallick <snyxmk@gmail.com>
2023-10-23 18:28:27 +0530

    fixed the function name

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use serde_json;
 use std::env;
 
-fn decode_dencode_value(encoded_value: &str) -> serde_json::Value {
+fn decode_bencode_value(encoded_value: &str) -> serde_json::Value {
     if let Some((len, rest)) = encoded_value.split_once(':') {
         if let Ok(len) = len.parse::<usize>() {
             return serde_json::Value::String(rest[..len].to_string());
@@ -15,10 +15,8 @@ fn main() {
     let command = &args[1];
 
     if command == "decode" {
-        println!("Logs");
-
         let encoded_value = &args[2];
-        let decoded_value = decode_dencode_value(encoded_value);
+        let decoded_value = decode_bencode_value(encoded_value);
         println!("{}", decoded_value.to_string())
     } else {
         println!("Unknown command: {}", args[1])
